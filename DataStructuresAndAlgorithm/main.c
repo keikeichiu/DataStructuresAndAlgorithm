ce74982f6efb512dd4859af3d523cfbde17d6fd0
keikeichiu <zhaoyongqi0911@proton.me>
2024-11-01 13:29:43 +0800

    Initial Commit

---
+++ DataStructuresAndAlgorithm/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DataStructuresAndAlgorithm
+//
+//  Created by tom on 1/11/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
